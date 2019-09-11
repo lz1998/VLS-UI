778131a804c09a163257ed42b6d862e20397d659
李政 <875543533@qq.com>
2019-09-11 23:32:34 +0800

    修改样式

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,13 +2,13 @@
   <div class="home">
     <el-row class="header">
       <el-col :span="6" class="header-item">
-
+        <div class="wrapper-in"></div>
       </el-col>
       <el-col :span="12" class="header-item">
-
+        <div class="wrapper-in"></div>
       </el-col>
       <el-col :span="6" class="header-item">
-
+        <div class="wrapper-in"></div>
       </el-col>
     </el-row>
     <el-row>
@@ -25,7 +25,7 @@
       </el-col>
       <el-col :span="12">
         <el-row class="wrapper-map-out">
-          <div class="wrapper-map-in"></div>
+          <div class="wrapper-in"></div>
         </el-row>
         <el-row>
           <el-col :span="12" class="wrapper-out">
@@ -74,31 +74,24 @@ export default {
   .wrapper-out{
     width: 25vw;
     height:30vh;
+    padding: 1vh 1vw;
     background-color: orange;
   }
   .wrapper-in{
-    width:23vw;
-    height:28vh;
-    margin-top:1vh;
-    margin-left: 1vw;
+    width:100%;
+    height:100%;
     background-color: aqua;
   }
   .wrapper-map-out{
     width: 50vw;
     height:60vh;
+    padding:1vh 1vw;
     background-color: orange;
   }
-  .wrapper-map-in{
-    width: 48vw;
-    height:58vh;
-    margin-top:1vh;
-    margin-left: 1vw;
-    background-color: aqua;
-  }
+
   .header-item{
     height: 100%;
+    padding:0 1vw;
     background-color: red;
-    margin-left: 1vh;
-
   }
 </style>
