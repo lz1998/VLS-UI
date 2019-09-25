ed88f98fe29ff6d505a11fed24ca44caca7e8626
李政 <875543533@qq.com>
2019-09-26 00:27:03 +0800

    编辑图表option树形控件

--- src/views/chartManage/index.vue
+++ src/views/chartManage/index.vue
@@ -32,9 +32,21 @@
         <!--新增/编辑图表对话框-->
         <el-dialog :visible.sync="chartDialogVisible" :title="isAddOperation?'新增图表':'编辑图表'">
             <el-form>
-                <el-form-item :label-width="chartFormLabelWidth" label="标题">
+                <el-form-item :label-width="chartFormLabelWidth" label="图表标题">
                     <el-input></el-input>
                 </el-form-item>
+                <el-form-item :label-width="chartFormLabelWidth" label="数据源URL">
+                    <el-input></el-input>
+                </el-form-item>
+                <el-tree :data="treeData" ref="optionTree">
+                    <span class="custom-tree-node" slot-scope="{ node, data }">
+                        <span style="width: 120px;display: inline-block;text-align: left;" @click="currentOptionDataId=node.id">{{ node.label}}</span>
+                        <span v-if="currentOptionDataId!=node.id && node.value!=null">{{node.value}}</span>
+                        <span v-if="currentOptionDataId==node.id && node.isLeaf">
+                            <input size="mini" v-model="data.value"></input>
+                        </span>
+                    </span>
+                </el-tree>
                 <el-form-item>
                     <el-button v-if="isAddOperation" type="primary" @click="addChart">保存</el-button>
                     <el-button v-if="!isAddOperation" type="primary" @click="updateChart">保存</el-button>
@@ -75,15 +87,249 @@
                         option:{}
                     }
                 ],
+
                 isAddOperation:true, // true是新增，false是编辑
                 chartDialogVisible:false, // 显示对话框
-                chartFormLabelWidth:'120px'
+                chartFormLabelWidth:'120px',
+                chartOption:{
+                    title:{
+                        show:null,
+                        text:null,
+                        textStyle:{
+                            color:null,
+                            fontStyle:null,
+                            fontWeight:null,
+                            fontSize:null,
+                            lineHeight:null,
+                            width:null,
+                            height:null
+                        },
+                        subText:null,
+                        subtextStyle:{
+                            color:null,
+                            fontStyle:null,
+                            fontWeight:null,
+                            fontSize:null,
+                            lineHeight:null,
+                            width:null,
+                            height:null
+                        },
+                        textAlign:null,
+                        padding:null,
+                        left:null,
+                        top:null,
+                        right:null,
+                        bottom:null,
+                        backgroundColor:null,
+                        borderColor:null,
+                        borderWidth:null,
+                        borderRadius:null
+                    },
+                    legend:{
+                        show:null,
+                        left:null,
+                        top:null,
+                        right:null,
+                        bottom:null,
+                        width:null,
+                        height:null,
+                        orient:null,
+                        align:null,
+                        padding:null,
+                        itemGap:null,
+                        itemWidth:null,
+                        itemHeight:null,
+                        textStyle:{
+                            color:null,
+                            fontStyle:null,
+                            fontWeight:null,
+                            fontSize:null,
+                            lineHeight:null,
+                            width:null,
+                            height:null
+                        },
+                        icon:null,
+                        backgroundColor:null,
+                        borderColor:null,
+                        borderWidth:null,
+                        borderRadius:null
+                    },
+                    grid:{
+                        show:null,
+                        left:null,
+                        top:null,
+                        right:null,
+                        bottom:null,
+                        width:null,
+                        height:null,
+                        backgroundColor:null,
+                        borderColor:null,
+                        borderWidth:null
+                    },
+                    xAxis:{
+                        show:null,
+                        position:null,
+                        type:null,
+                        name:null,
+                        nameLocation:null,
+                        nameTextStyle:{
+                            color:null,
+                            fontStyle:null,
+                            fontWeight:null,
+                            fontSize:null,
+                            lineHeight:null,
+                            width:null,
+                            height:null
+                        },
+                        nameGap:null,
+                        splitNumber:null,
+                        axisLine:{
+                            show:null,
+                            symbol:null,
+                            symbolSize:null,
+                            symbolOffset:null,
+                            lineStyle:{
+                                color:null,
+                                width:null,
+                                type:null
+                            }
+                        },
+                        axisTick:{
+                            show:null,
+                            interval:null,
+                            inside:null,
+                            length:null,
+                            lineStyle:{
+                                color:null,
+                                width:null,
+                                type:null
+                            }
+                        },
+                        axisLabel:{
+                            show:null,
+                            interval:null,
+                            inside:null,
+                            rotate:null,
+                            margin:null,
+                            color:null,
+                            fontStyle:null,
+                            fontWeight:null,
+                            fontSize:null,
+                            lineHeight:null,
+                            backgroundColor:null,
+                            borderColor:null,
+                            borderWidth:null,
+                            borderRadius:null,
+                            padding:null,
+                            width:null,
+                            height:null
+                        }
+                    },
+                    yAxis:{
+                        show:null,
+                        position:null,
+                        type:null,
+                        name:null,
+                        nameLocation:null,
+                        nameTextStyle:{
+                            color:null,
+                            fontStyle:null,
+                            fontWeight:null,
+                            fontSize:null,
+                            lineHeight:null,
+                            width:null,
+                            height:null
+                        },
+                        nameGap:null,
+                        splitNumber:null,
+                        axisLine:{
+                            show:null,
+                            symbol:null,
+                            symbolSize:null,
+                            symbolOffset:null,
+                            lineStyle:{
+                                color:null,
+                                width:null,
+                                type:null
+                            }
+                        },
+                        axisTick:{
+                            show:null,
+                            interval:null,
+                            inside:null,
+                            length:null,
+                            lineStyle:{
+                                color:null,
+                                width:null,
+                                type:null
+                            }
+                        },
+                        axisLabel:{
+                            show:null,
+                            interval:null,
+                            inside:null,
+                            rotate:null,
+                            margin:null,
+                            color:null,
+                            fontStyle:null,
+                            fontWeight:null,
+                            fontSize:null,
+                            lineHeight:null,
+                            backgroundColor:null,
+                            borderColor:null,
+                            borderWidth:null,
+                            borderRadius:null,
+                            padding:null,
+                            width:null,
+                            height:null
+                        }
+                    },
+                    backgroundColor:null
+                },
+                treeData:[],
+                currentOptionDataId:''
             }
         },
         methods:{
+            // test()
             loadData(){
                 // 在这里读取chartList
             },
+            // 根据option生成树形控件
+            constructTreeData(myjson,id){
+                const list=[]
+                for(let key in myjson){
+                    const obj={}
+                    obj.label=key
+                    if(id==''){
+                        obj.id=key
+                    }else{
+                        obj.id=id+'.'+key
+                    }
+                    obj.value=null
+
+                    if(myjson[key]!=null){
+                        obj.children=this.constructTreeData(myjson[key],obj.id)
+                    }
+                    list.push(obj)
+                }
+                // console.log(list)
+                return list
+            },
+            // 根据树形控件data生成option
+            constructOptionData(mylist){
+                const obj={}
+                mylist.forEach(item=>{
+                    console.log(item.label)
+                    if(item.children==undefined){
+                        obj[item.label]=item.value
+                    }else{
+                        obj[item.label]=this.constructOptionData(item.children)
+                    }
+                })
+                return obj
+            },
+
             queryChart(){
 
             },
@@ -102,6 +348,7 @@
             },
             addChart(){
                 this.chartDialogVisible=false
+                console.log(this.constructOptionData(this.treeData))
 
             },
             updateChart(){
@@ -109,6 +356,10 @@
 
             }
 
+        },
+        created() {
+            this.loadData()
+            this.treeData=this.constructTreeData(this.chartOption,'')
         }
     }
 </script>
