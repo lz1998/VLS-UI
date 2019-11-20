4ffa5f11a5adfff1a57946244c5db1ffb60c363e
misssonder <819643718@qq.com>
2019-11-20 20:38:22 +0800

    修改顶部栏

--- src/views/layout/Layout.vue
+++ src/views/layout/Layout.vue
@@ -5,9 +5,9 @@
                 <MyMenu></MyMenu>
             </el-aside>
             <el-container style="height: 100vh">
-                <el-header style="border-bottom: 1px solid #aaa;">
+                <el-header style="border-bottom: 1px solid #aaa;background-color:#DCDFE6">
                     <div class="title">可视化数据大屏管理后台</div>
-                    <div class="logout" @click="handleLogout">退出</div>
+                    <el-button @click="handleLogout" class="logout" >登出<i class="el-icon-right"></i></el-button>
                 </el-header >
                 <el-main >
                     <router-view/>
@@ -41,6 +41,7 @@
         height: 100vh;
         width: 100%;
         .title{
+            color: #303133;
             float: left;
             height: 60px;
             line-height: 60px;
@@ -49,11 +50,12 @@
         }
         .logout{
             float: right;
-            height: 60px;
-            line-height: 60px;
-            font-size: 24px;
+            background:#DCDFE6;
+            height: 30px;
+            line-height: 30px;
+            font-size: 20px;
             padding-left: 20px;
-            border-left: 1px solid #ccc;
+            border: 5px solid #DCDFE6;
         }
 
     }
